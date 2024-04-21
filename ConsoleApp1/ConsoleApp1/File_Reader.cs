09a3cb7b90e8794b6eb2f79c0272f7cc6e04ea8b
s27051 <s27051@pjwstk.edu.pl>
2024-04-21 22:10:05 +0200

    Added ReafFile method

--- ConsoleApp1/ConsoleApp1/File_Reader.cs
+++ ConsoleApp1/ConsoleApp1/File_Reader.cs
@@ -1,6 +1,9 @@
 namespace ConsoleApp1;
 
-public class File_Reader
+public class File_Reader : IFile_Reader
 {
-    
+    public override List<KeyValuePair<string, List<string>>> ReadFile(string path)
+    {
+        throw new NotImplementedException();
+    }
 }

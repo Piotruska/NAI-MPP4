cee471a1921b91ff043ad52d810e62823793eecb
s27051 <s27051@pjwstk.edu.pl>
2024-04-21 22:25:03 +0200

    Finished Reading method in File_Reader.cs

--- ConsoleApp1/ConsoleApp1/File_Reader.cs
+++ ConsoleApp1/ConsoleApp1/File_Reader.cs
@@ -2,8 +2,26 @@ namespace ConsoleApp1;
 
 public class File_Reader : IFile_Reader
 {
-    public override List<KeyValuePair<string, List<string>>> ReadFile(string path)
+    public override List<Tuple<string,List<string>>> ReadFile(string filePath)
     {
-        throw new NotImplementedException();
+        List<Tuple<string,List<string>>> data = new List<Tuple<string,List<string>>>();
+        try {
+            using (StreamReader reader = new StreamReader(filePath)) {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var l = line.Split(",");
+                    string d = l[0];
+                    List<string> att = new List<string>(l.Skip(1));
+                    data.Add(Tuple.Create(d,att));
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        catch (Exception e) {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+        return data;
     }
 }

2912bec4222cd5ff3f3185f751051d956beed28f
s27051 <s27051@pjwstk.edu.pl>
2024-04-21 23:00:05 +0200

    Edited the BayesNaiveClaffifier.cs constructor

--- ConsoleApp1/ConsoleApp1/BayesNaiveClaffifier.cs
+++ ConsoleApp1/ConsoleApp1/BayesNaiveClaffifier.cs
@@ -6,11 +6,16 @@ public class BayesNaiveClaffifier
     public readonly List<Tuple<string, List<string>>> testData;
     private Igraphics _graphics;
 
+    private Dictionary<string,List<Dictionary<string, double>>> _probabilities =
+        new Dictionary<string, List<Dictionary<string, double>>>();
+
     public BayesNaiveClaffifier(List<Tuple<string, List<string>>> trainData, List<Tuple<string, List<string>>> testData,Igraphics graphics)
     {
         this.trainData = trainData;
         this.testData = testData;
         _graphics = graphics;
+        _probabilities.Add("e",new List<Dictionary<string, double>>());
+        _probabilities.Add("p",new List<Dictionary<string, double>>());
     }
 
     public void Train()
